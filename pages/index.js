52421f2f7027d4c4aae9f85054294ff4064d69b5
Sidra Waseem <sidraw24@gmail.com>
2023-03-28 17:29:59 +0500

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,23 @@
 export default function Home() {
   return (
     <>
-      <div>
-        <div></div>
-        <div></div>
+      <div className="flexContainer">
+
+        <div className="firstColumn">
+        <h1>New Breakthrough in Cancer Research Offers Hope for More Effective Treatments</h1>
+        <p>Scientists and medical researchers around the world have been working tirelessly to find new and more effective treatments for cancer, one of the most devastating diseases of our time. </p>
+        <p>In a recent breakthrough, a team of researchers has identified a promising new target for cancer therapies that could help to improve outcomes for patients.</p>
+        <i>-Some random person</i>
+
+        <h1>Rising Inflation Sparks Concern Among Experts</h1>
+        <p>Inflation, the rate at which the general level of prices for goods and services is rising, has become a major concern for the global economy. </p>
+        <i>-Some random person</i>
+        </div>
+
+        <div className="secondColumn">
+        
+        </div>
+
         <div></div>
         <div></div>
       </div>
